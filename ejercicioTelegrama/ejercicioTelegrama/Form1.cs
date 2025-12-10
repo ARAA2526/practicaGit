3ec902071322cd9eb056bd0d60decf4a23a6607c
antonioandreuaraez <antandara@alu.edu.gva.es>
2025-12-10 23:44:24 +0100

    Indicamos botones radiales en vez del checkbox

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -10,19 +10,27 @@ namespace ejercicioTelegrama
         private void btnCalcularPrecio_Click(object sender, EventArgs e)
         {
             string textoTelegrama;
-            char tipoTelegrama = 'o';
+            char tipoTelegrama = 'o';  // Por defecto ordinario
             int numPalabras = 0;
             double coste = 0;
+
             //Leo el telegrama 
             textoTelegrama = txtTelegrama.Text;
-            // telegrama urgente? 
-            if (chkUrgente.Checked)
+
+            //Comprobación del tipo de telegrama usando los radiobuttons
+            if (rdiUrgente.Checked)
             {
                 tipoTelegrama = 'u';
             }
+            else if (rdiOrdinario.Checked)
+            {
+                tipoTelegrama = 'o';
+            }
+
             //Obtengo el número de palabras que forma el telegrama 
             string[] palabras = textoTelegrama.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             numPalabras = palabras.Length;
+
             //Si el telegrama es ordinario 
             if (tipoTelegrama == 'o')
             {
@@ -35,7 +43,6 @@ namespace ejercicioTelegrama
                     coste = 2.5 + 0.5 * (numPalabras - 10);
                 }
             }
-
             //Si el telegrama es urgente 
             else if (tipoTelegrama == 'u')
             {
@@ -48,8 +55,8 @@ namespace ejercicioTelegrama
                     coste = 5 + 0.75 * (numPalabras - 10);
                 }
             }
+
             txtPrecio.Text = coste.ToString() + " euros";
         }
-
     }
 }
